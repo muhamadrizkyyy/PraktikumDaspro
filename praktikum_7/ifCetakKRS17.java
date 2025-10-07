a39a3247035d947949353a91cd19db0d7ee3ef3b
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-10-07 16:02:31 +0700

    Tugas 1

--- praktikum_7/ifCetakKRS17.java
+++ praktikum_7/ifCetakKRS17.java
@@ -10,11 +10,13 @@ public class ifCetakKRS17 {
         System.out.print("Apakah UKT sudah lunas: ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu”");
+        // }
+
+        System.out.println(uktLunas ? "Pembayaran UKT terverifikasi" : "Registrasi ditolak, silahkan lunasi ukt terlebih dahulu");
     }
 }
