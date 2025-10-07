37161083cb6b791ee2c89f9cfbabad5184d9fdb3
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-10-07 14:28:53 +0700

    Percobaan 1

---
+++ praktikum_7/ifCetakKRS17.java
@@ -0,0 +1,18 @@
+package praktikum_7;
+
+import java.util.Scanner;
+
+public class ifCetakKRS17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas: ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
