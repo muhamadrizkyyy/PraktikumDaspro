3444e79ee19cef2c01a0667cc19ee7e5dc92b11b
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-10-21 13:07:57 +0700

    Modifikasi percobaan 1

--- praktikum_8/SIAKADFor17.java
+++ praktikum_8/SIAKADFor17.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SIAKADFor17 {
     public static void main(String[] args) {
         double nilai, terendah =100, tertinggi = 0;
+        int jml_mhs_lulus = 0, jml_mhs_tdkLulus = 0;
 
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i <= 10; i++) {
@@ -14,9 +15,17 @@ public class SIAKADFor17 {
             } else if(nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai < 60) {
+                jml_mhs_tdkLulus++;
+            } else {
+                jml_mhs_lulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi adalah " + tertinggi);
         System.out.println("Nilai terendah adalah " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus adalah " + jml_mhs_lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus adalah " + jml_mhs_tdkLulus);
     }
 }
