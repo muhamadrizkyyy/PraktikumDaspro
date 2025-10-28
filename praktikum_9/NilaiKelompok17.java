f35e9a293e351192e2c6aae7a01a2a3c4832e5a0
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-10-28 13:48:21 +0700

    Modifikasi percobaan 4

--- praktikum_9/NilaiKelompok17.java
+++ praktikum_9/NilaiKelompok17.java
@@ -4,7 +4,7 @@ public class NilaiKelompok17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 1;
-        int totalNilai, nilai, rataNilai;
+        int totalNilai, nilai, rataNilai = 0, nilaiTertinggi = 0, kelompokTertinggi = 0;
         while (i <= 5) {
             System.out.println("Kelompok " + i);
 
@@ -16,8 +16,16 @@ public class NilaiKelompok17 {
             }
 
             rataNilai = totalNilai / 5;
+
+            if (rataNilai > nilaiTertinggi) {
+                nilaiTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             System.out.println("Kelompok " + i + " : Rata-rata nilai kelompok " + i + " adalah " + rataNilai);
             i++;
         }
+
+        System.out.println("Kelompok dengan rata-rata nilai tertinggi adalah kelompok " + kelompokTertinggi + " dengan rata-rata nilai " + nilaiTertinggi);
     }
 }
