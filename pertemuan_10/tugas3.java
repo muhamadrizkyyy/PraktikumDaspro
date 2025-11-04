1b0cb030fb7c019e1392d5c2249e74be35189cd9
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-04 16:02:19 +0700

    perbaiki

--- pertemuan_10/tugas3.java
+++ pertemuan_10/tugas3.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class tugas3 {
     public static void main(String[] args) {
         String[] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"};
-
+        boolean isFound = false;
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nama menu yang dicari: ");
         String cari = sc.nextLine();
 
         for(String i : menu) {
             if (i.equalsIgnoreCase(cari)) {
-                System.out.println("menu tersedia!");
-                break;
-            } else {
-                System.out.println("menu tidak tersedia");
+                isFound = true;
                 break;
             }
+        }
+
+        if (!isFound) {
+            System.out.println("menu tidak tersedia!");
+        } else {
+            System.out.println("menu tersedia!");
         }
     }
 }
