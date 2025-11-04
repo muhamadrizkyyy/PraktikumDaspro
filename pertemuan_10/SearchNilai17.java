3a440707d95371de3ebd127a221fd3f836b587f5
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-04 14:24:28 +0700

    Modifikasi soal

--- pertemuan_10/SearchNilai17.java
+++ pertemuan_10/SearchNilai17.java
@@ -1,18 +1,37 @@
+import java.util.Scanner;
+
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int[] nilai = { 90, 85, 95, 80, 75 };
-        int key = 95;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc.nextInt();
+
+        int[] nilai = new int[jmlMhs];
+
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai[i] = sc.nextInt();
+        }
+        
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
         int hasil = 0;
 
         for (int i = 0; i < nilai.length; i++) {
             if (nilai[i] == key) {
                 hasil = i;
+
+                System.out.println();
+                System.out.println("Nilai " + key + " ditemukan yang merupakan nilai mahasiswa ke-" + hasil);
+                System.out.println();
+                break;
+            } else {
+                System.out.println();
+                System.out.println("Nilai yang dicari tidak ditemukan!");
+                System.out.println();
                 break;
             }
         }
-
-        System.out.println();
-        System.out.println("Nilai " + key + " ditemukan pada indeks ke-" + hasil);
-        System.out.println();
     }
 }
