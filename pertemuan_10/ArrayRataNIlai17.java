1de9810973d66de255ad322028f5e90e871541da
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-04 13:51:40 +0700

    Modifikasi soal nomor 1

--- pertemuan_10/ArrayRataNIlai17.java
+++ pertemuan_10/ArrayRataNIlai17.java
@@ -4,7 +4,7 @@ public class ArrayRataNIlai17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
-        double total = 0, rata2;
+        double total = 0, rata2, mhsLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke- " + (i+1) +" : ");
@@ -13,9 +13,14 @@ public class ArrayRataNIlai17 {
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+
+            if (nilaiMhs[i] > 70) {
+                mhsLulus++;
+            }
         }
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai mahasiswa adalah : " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus adalah : " + mhsLulus);
     }
 }
