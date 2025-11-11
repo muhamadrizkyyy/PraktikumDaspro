4684b6235aa88b4d185405458f5c05df2e24330d
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-11 11:29:54 +0700

    modifikasi soal nomor 5

--- pertemuan_11/Bioskop17.java
+++ pertemuan_11/Bioskop17.java
@@ -10,16 +10,21 @@ public class Bioskop17 {
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
-        System.out.println("\n");
-        System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + i + " = " + penonton[i].length);
+        }
+
+        // System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+        // System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+        // System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+        // System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        // System.out.println("\n");
+        // System.out.println(penonton.length);
+        // System.out.println(penonton[0].length);
+        // System.out.println(penonton[1].length);
+        // System.out.println(penonton[2].length);
+        // System.out.println(penonton[3].length);
     }
 }
