a8a388fe884eca8bd6d9572140bd0aa7d9d53360
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-11 11:49:05 +0700

    modifikasi soal nomor 9

--- pertemuan_11/Bioskop17.java
+++ pertemuan_11/Bioskop17.java
@@ -15,17 +15,21 @@ public class Bioskop17 {
         //     System.out.println("Panjang baris ke-" + i + " = " + penonton[i].length);
         // }
 
-        for (String[] pnt : penonton) {
-            System.out.println("Panjang baris : " + pnt.length);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + " = " + String.join(", ", penonton[i]));
         }
 
-        System.out.println("Penonton pada baris ke-3");
+        // for (String[] pnt : penonton) {
+        //     System.out.println("Panjang baris : " + pnt.length);
+        // }
+
+        // System.out.println("Penonton pada baris ke-3");
         // for (int i = 0; i < penonton[2].length; i++) {
         //     System.out.println(penonton[2][i]);
         // }
-        for (String pnt : penonton[2]) {
-            System.out.println(pnt);
-        }
+        // for (String pnt : penonton[2]) {
+        //     System.out.println(pnt);
+        // }
 
         // System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         // System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
