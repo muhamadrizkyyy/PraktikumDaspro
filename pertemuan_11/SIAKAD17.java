0298d6ca6bb65afcc6853b54cf97145bec7319b1
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-11 14:05:10 +0700

    modifikasi soal percobaan 4

--- pertemuan_11/SIAKAD17.java
+++ pertemuan_11/SIAKAD17.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class SIAKAD17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
+        System.out.print("Jumlah mahasiswa: ");
+        int jmlMhs = input.nextInt();
+        System.out.print("Jumlah matkul: ");
+        int jmlMtkl = input.nextInt();
+        int[][] nilai = new int[jmlMhs][jmlMtkl];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
@@ -23,14 +27,14 @@ public class SIAKAD17 {
         System.out.println("\n=============================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < jmlMtkl; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < nilai.length; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jmlMhs);
         }
     }
 }
