7f9a46956c1cf28d13c446d2fd27c3c744b5ef44
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-25 14:46:52 +0700

    modifikasi soal nomor 3 trial 3

--- pertemuan_14/kafe17.java
+++ pertemuan_14/kafe17.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class kafe17 {
     public static void main(String[] args) {
-        menu("Budi", true, "DISKON30");
+        Scanner sc = new Scanner(System.in);
+        String kodePromo = "DISKON50";
+
+        menu("Budi", true, kodePromo);
+
+        System.out.print("Pilih menu: ");
+        int pilih = sc.nextInt();
+        System.out.print("Quantity: ");
+        int qty = sc.nextInt();
+
+        int totalBayar = hitungTotalHarga(pilih, qty, kodePromo);
+        System.out.println("Total Bayar: Rp " + totalBayar);
     }
 
     public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
@@ -26,6 +39,19 @@ public class kafe17 {
         System.out.println("5. Roti Bakar – Rp 10,000");
         System.out.println("6. Mie Goreng – Rp 18,000");
         System.out.println("===========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    }
+
+    public static int hitungTotalHarga(int pilih, int qty, String kodePromo) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        double diskon = 0;
+
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            diskon = 0.5;
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            diskon = 0.3;
+        }
+
+        int hargaTotal = diskon != 0 ? (int) ((hargaItems[pilih - 1] * qty) * (1 - diskon)) : hargaItems[pilih - 1] * qty;
+        return hargaTotal;
     }
 }
