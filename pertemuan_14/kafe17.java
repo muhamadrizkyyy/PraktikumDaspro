5541b40a5536f57bea7cfd6aa0280549b4edad5a
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-11-25 14:11:44 +0700

    modifikasi percobaan 2

--- pertemuan_14/kafe17.java
+++ pertemuan_14/kafe17.java
@@ -1,17 +1,15 @@
 public class kafe17 {
     public static void main(String[] args) {
-        System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam – Rp 15,000");
-        System.out.println("2. Cappuccino – Rp 20,000");
-        System.out.println("3. Latte – Rp 22,000");
-        System.out.println("4. Teh Tarik – Rp 12,000");
-        System.out.println("5. Roti Bakar – Rp 10,000");
-        System.out.println("6. Mie Goreng – Rp 18,000");
-        System.out.println("===========================");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        menu("Andi", true);
     }
 
-    public static void menu() {
+    public static void menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat Datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam – Rp 15,000");
         System.out.println("2. Cappuccino – Rp 20,000");
