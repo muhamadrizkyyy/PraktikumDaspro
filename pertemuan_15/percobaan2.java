de9e2abe3c13f41b60bbef6f2c3e7f5af7420918
muhamadrizkyhamdani <muhamadrizkyhamdanii@gmail.com>
2025-12-02 13:41:07 +0700

    percobaan 2

---
+++ pertemuan_15/percobaan2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class percobaan2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int bilangan = sc.nextInt();
+        System.out.print("Masukkan pangkat: ");
+        int pangkat = sc.nextInt();
+        System.out.println("Hasil: " + hitungPangkat(bilangan, pangkat));
+    }
+
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return 1;
+        } else {
+            return (x * hitungPangkat(x, y - 1));
+        }
+    }
+}
